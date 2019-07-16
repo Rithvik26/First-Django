1a8d8dc6f68ac6addb1bebe36b0bb5a373d0dcd3
Rithvik26 <37191570+Rithvik26@users.noreply.github.com>
2019-07-16 15:17:02 +0530

    com

--- notifywebsite/urls.py
+++ notifywebsite/urls.py
@@ -24,4 +24,9 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     url(r'^admin/', admin.site.urls),
+
+    url(r'^music/',include('music.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
